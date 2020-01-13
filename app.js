af42a925c8769832f64e91e47efb1274d0c2100a
Dean Ma <mrdingma@gmail.com>
2020-01-13 10:59:24 -0800

    create game board

---
+++ app.js
@@ -0,0 +1,22 @@
+// TODO: add reset board button
+
+// first move: X
+
+window.gameboard = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
+
+// onClick game board:
+
+// set innerHTML of the element to be the current move
+
+// set the corresponding move on the game board
+
+// check if there is a winner
+
+
+// on Click reset board
+// clear the innerHTML of all the elements
